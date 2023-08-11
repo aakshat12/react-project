202071096ba153d9b0984b6f57b18c17f0cf3324
aakshat12 <118069569+aakshat12@users.noreply.github.com>
2023-08-11 16:53:46 +0530

    Delete src directory

--- src/redux/action/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import handleCart from "./handelCart";
-import {combineReducers} from "redux";
-
-const rootReducers = combineReducers({
-    handleCart,
-})
-
-export default rootReducers;
-    
--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
- import {configureStore} from 'redux'
- import rootReducers from './redux/action/reducer'
-
- const store = configureStore(rootReducers);
-
- export default store;
