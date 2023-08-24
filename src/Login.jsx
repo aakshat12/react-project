c71e14d96e1ef876fd8ab8a5d937f4f09eb5e662
aakshat12 <118069569+aakshat12@users.noreply.github.com>
2023-08-24 16:48:22 +0530

    Add files via upload

---
+++ src/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div className='login'>
+        <form>
+            <label for='chk' aria-hidden='true'>Login</label>
+            <input type="email" name="email" placeholder='Email' required=""/>
+            <input type="password" name="pswd" placeholder='Password' required=""/>
+            <button>Login</button>
+
+        </form>
+    </div>
+  )
+}
+export default Login;
