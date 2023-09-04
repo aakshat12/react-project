3fd854eb59ce81a77b3a59431ee83cce1ad0eba4
aakshat12 <118069569+aakshat12@users.noreply.github.com>
2023-09-04 17:10:58 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+export const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate()
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/login", {  email, password })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+      navigate('/home')
+  };
+  
+
+
+  return (
+    <div className='login'>
+        <form onSubmit={handleSubmit}>
+            <label for='chk' aria-hidden='true'>Login</label>
+            <input type="email" name="email" placeholder='Email' required="" onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" name="pswd" placeholder='Password' required=""  onChange={(e) => setPassword(e.target.value)}/>
+            <button>Login</button>
+
+        </form>
+    </div>
+  )
+}
+export default Login;
