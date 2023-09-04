42019e67d1250bf8863a12cdd5cf431316d52091
aakshat12 <118069569+aakshat12@users.noreply.github.com>
2023-09-04 17:13:28 +0530

    Add files via upload

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/login", {  email, password })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+      navigate('/home')
+  };
+  
+
+
   return (
     <div className='login'>
-        {/* <form>
+        <form onSubmit={handleSubmit}>
             <label for='chk' aria-hidden='true'>Login</label>
-            <input type="email" name="email" placeholder='Email' required=""/>
-            <input type="password" name="pswd" placeholder='Password' required=""/>
+            <input type="email" name="email" placeholder='Email' required="" onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" name="pswd" placeholder='Password' required=""  onChange={(e) => setPassword(e.target.value)}/>
             <button>Login</button>
 
-        </form> */}
+        </form>
     </div>
   )
 }
